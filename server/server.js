b2ef73afbcafa4d5790462e79951f8389efe33d4
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-24 10:29:00 -0500

    final changes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-let numbersReceived;
-let finalAnswer;
-
-let storeCalcs = [];
+let storeCalcs = [
+	{
+		valueOne: 23,
+		value: 2,
+		answer: 23,
+	},
+];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,30 +20,9 @@ app.get('/calculations', (req, res) => res.send(storeCalcs));
 app.post('/calculations', (req, res) => {
 	console.log(req.body);
 
-	storeCalcs.push(req.body);
-	numbersReceived = req.body;
-	mathTime(numbersReceived);
-	storeCalcs[storeCalcs.length - 1].answer = finalAnswer;
-	res.sendStatus(201);
-	console.log(storeCalcs[storeCalcs.length - 1]);
-});
+	//? push the req.body
 
-function mathTime(numbers) {
-	let numberOne = Number(numbers.numbersArray[0]);
-	let numberTwo = Number(numbers.numbersArray[1]);
-	if (numbers.operator === '+') {
-		finalAnswer = numberOne + numberTwo;
-		return finalAnswer;
-	} else if (numbers.operator === '-') {
-		finalAnswer = numberOne - numberTwo;
-		return finalAnswer;
-	} else if (numbers.operator === '/') {
-		finalAnswer = numberOne / numberTwo;
-		return finalAnswer;
-	} else if (numbers.operator === '*') {
-		finalAnswer = numberOne * numberTwo;
-		return finalAnswer;
-	}
-}
+	res.sendStatus(201);
+});
 
 app.listen(PORT, () => console.log('listening on port', PORT));
