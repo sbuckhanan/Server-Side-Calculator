96c183f5a4c09f9398447f1c0ac602476536c4bb
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-23 21:17:36 -0500

    start get and post on server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 5000;
+
+let storeCalcs = [
+	{
+		valueOne: 23,
+		value: 2,
+		answer: 23,
+	},
+];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => console.log('Hello World!'));
+
+app.get('/calculations', (req, res) => res.send(storeCalcs));
+app.post('/calculations', (req, res) => {
+	console.log(req.body);
+
+	//? push the req.body
+
+	res.sendStatus(201);
+});
 
 app.listen(PORT, () => console.log('listening on port', PORT));
