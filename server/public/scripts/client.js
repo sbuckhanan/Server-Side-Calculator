48d76afbe6173c0e817d29abb431b76540683350
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-23 21:07:12 -0500

    sudo code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,15 @@ $(onReady);
 function onReady() {
 	console.log('We are ready');
 }
+
+//? get value of input field
+//? update input field with number or symbol that was clicked
+//? click listener for each button
+//? equal button is like a submit button
+//? clear button should clear input and add that info to the ul
+//? should do a post request when you click enter
+//? post request should add an object to the server that gets added to the array
+//? maybe send back the request (req.body) and display the response to the dom instead of looping through a response array
+//? update the calculated field on the dom. that should also be stored on the server in that object
+
+let inputField = $('.numberInput');
