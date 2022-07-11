7ecb3eb00a2842b8014ad7b45fbd82988e5f3a46
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-11 13:32:31 -0500

    update PORT

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 let mathTime = require('./modules/mathstuff.js');
 
 let storeCalcs = [];
